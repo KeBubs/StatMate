b18af90962d0a65f29ca92ca0a19870be1fb784a
Kieran <kebusby@hotmail.co.uk>
2024-01-14 21:35:16 +0000

    Added dynamic routing for Leagues

--- statmate-nextjs/src/components/Header/header.tsx
+++ statmate-nextjs/src/components/Header/header.tsx
@@ -7,11 +7,11 @@ export default function Header () {
         <header className={Styles.container}>
             <h1 className={Styles.logo}>StatMate</h1>
             <Link className={Styles.link} href='/todays-fixtures'>Todays Fixtures</Link>
-            <Link className={Styles.link} href='/premier-league'>Premier League</Link>
-            <Link className={Styles.link} href='/bundesliga'>Bundesliga</Link>
-            <Link className={Styles.link} href='/la-liga'>La Liga</Link>
-            <Link className={Styles.link} href='/ligue-1'>Ligue 1</Link>
-            <Link className={Styles.link} href='/serie-a'>Serie A</Link>
+            <Link className={Styles.link} href='/league/premier-league'>Premier League</Link>
+            <Link className={Styles.link} href='/league/bundesliga'>Bundesliga</Link>
+            <Link className={Styles.link} href='/league/la-liga'>La Liga</Link>
+            <Link className={Styles.link} href='/league/ligue-1'>Ligue 1</Link>
+            <Link className={Styles.link} href='/league/serie-a'>Serie A</Link>
         </header>
     )
 }
