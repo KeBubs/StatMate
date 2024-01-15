f2e619051bcb8c80ccce56fdcb145358efbb812b
Kieran <kebusby@hotmail.co.uk>
2024-01-15 14:43:34 +0000

    Added Under Construction text

--- statmate-nextjs/src/app/page.tsx
+++ statmate-nextjs/src/app/page.tsx
@@ -9,8 +9,7 @@ export default function Home() {
           <h1>StatMate</h1>
           <p>StatMate is a web app that provides football statistics for the top 5 European leagues.</p>
           <p>It is built using Next.js and Tailwind CSS.</p>
-          <p>It uses the <a href="https://www.football-data.org/">Football Data API</a> to retrieve the data.</p>
-          <p>It is hosted on <a href="https://vercel.com/">Vercel</a>.</p>
+          <p>The Website is currently under construction at the moment. Please stay tuned!</p>
         </section>
         
       
