aa608cbd5a2e161e79c68140f4700658fa898896
Kieran <kebusby@hotmail.co.uk>
2024-01-16 15:37:12 +0000

    Added helper function to landing page

--- statmate-nextjs/src/app/todays-fixtures/page.jsx
+++ statmate-nextjs/src/app/todays-fixtures/page.jsx
@@ -11,49 +11,45 @@ const authToken = process.env.API_KEY
 
 
 export default async function TodaysFixtures () {
-
-
-    // Function to fetch all matches for today.
-    
-    
     const Data = await fetchMatches(todaysMatches, authToken)
     const matches = Data.matches
-    console.log('The returned API data is: ', Data)
     
     return (
         <>
         <Header/>
         <main className={Styles.mainContainer}>
             <div className={Styles.container}>
-            {matches.map((match, index) => (
-        <section key={index} className={Styles.fixture}>
-            <img className={Styles.crest} src={match.homeTeam.crest} alt={`Home Team Crest`} />
-            <p>{`${match.homeTeam.name}`}</p>
-            {match.score.fullTime.home >= 0 ? 
-            ( 
-            <>
-            <p>{match.score.fullTime.home}</p>
-            <p>{match.score.fullTime.away}</p>
-            </>
-            )
-            : 
-            (
-            <>
-            <p>{match.utcDate}</p>
-            <p>{match.utcDate}</p>
-            </> 
-            )
-        }
-            
-            <p>{match.awayTeam.name}</p>
-            <img className={Styles.crest} src={match.awayTeam.crest} alt={`Away Team Crest`} />
-    </section>
-))}
-               
+            {matches.map((match, index) => {
+                const timeRegex = /T(\d{2}:\d{2}:\d{2})Z/;
+                const matchTime = match.utcDate.match(timeRegex);
+                const time = matchTime ? matchTime[1] : '';
+
+                return (
+                    <section key={index} className={Styles.fixture}>
+                        <img className={Styles.crest} src={match.homeTeam.crest} alt={`Home Team Crest`} />
+                        <p>{`${match.homeTeam.name}`}</p>
+                        {match.score.fullTime.home == (typeof(number)) ? 
+                        ( 
+                        <>
+                            <p>{match.score.fullTime.home}</p>
+                            <p>{match.score.fullTime.away}</p>
+                        </>
+                        )
+                        : 
+                        (
+                        <>
+                            <p>{time}</p>
+                        </>
+                        )
+                        }
+                        
+                        <p>{match.awayTeam.name}</p>
+                        <img className={Styles.crest} src={match.awayTeam.crest} alt={`Away Team Crest`} />
+                    </section>
+                )
+            })}
             </div>
         </main>
-            
-            
         </>
     )
 }
--- statmate-nextjs/src/functions/helpers.tsx
+++ statmate-nextjs/src/functions/helpers.tsx
@@ -1,6 +1,7 @@
 // Do I need to import .env file?
 import 'dotenv/config'
 
+// Function to fetch matches, requires endpoint URL and Auth Token passed in as an argument
 export async function fetchMatches(url: string, token: string) {
     const response = await fetch(url, {
     headers: {
