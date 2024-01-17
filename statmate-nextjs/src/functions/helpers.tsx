336f16516d0fd7ce5238ffcb75ab887de9b09cf5
Kieran <kebusby@hotmail.co.uk>
2024-01-17 17:35:49 +0000

    updated helpers.tsx

--- statmate-nextjs/src/functions/helpers.tsx
+++ statmate-nextjs/src/functions/helpers.tsx
@@ -4,6 +4,9 @@ const token = process.env.API_KEY
 
 // Function to fetch today's matches, requires endpoint URL and Auth Token passed in as an argument
 export async function fetchMatches() {
+    if (!token) {
+        throw new Error('API_KEY is not set');
+    }
     const url = `https://api.football-data.org/v4/matches`
     const response = await fetch(url, {
     headers: {
@@ -17,6 +20,9 @@ return todaysData;
 // Function to fetch League Standings to generate a leaderboard, requires endpoint URL, Auth Token passed in as argument's.
 // Assumes the league code has already been appended to the URL.
 export async function fetchStandings(code: string | null) {
+    if (!token) {
+        throw new Error('API_KEY is not set');
+    }
     const url = `https://api.football-data.org/v4/competitions/${code}/standings?season=2023`
     const response = await fetch(url, {
     method: "GET",
